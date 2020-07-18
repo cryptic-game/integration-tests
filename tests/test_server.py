0fb27c24b195944fbf06660d3b5da92ed6ca2f94
Defelo <elodef42@gmail.com>
2020-07-19 01:37:32 +0200

    Added tests for session action

--- tests/test_server.py
+++ tests/test_server.py
@@ -25,6 +25,22 @@ def setup_account():
     )
 
 
+def clear_sessions():
+    execute("DELETE FROM session")
+
+
+def setup_session() -> str:
+    token = uuid()
+    clear_sessions()
+    execute(
+        "INSERT INTO session (uuid, user, token, created, valid) VALUES (%s, %s, %s, current_timestamp, true)",
+        uuid(),
+        super_uuid,
+        token,
+    )
+    return token
+
+
 class TestServer(TestCase):
     def test_register_already_logged_in(self):
         setup_account()
@@ -90,6 +106,34 @@ class TestServer(TestCase):
         self.assertIsInstance(result, dict)
         self.assertEqual(["token"], list(result))
         self.assertTrue(is_uuid(result["token"]))
+
+    def test_session_already_logged_in(self):
+        setup_account()
+        client: Client = get_client()
+        client.login("super", super_password)
+
+        expected = {"error": "unknown action"}
+        actual = client.request({"action": "session", "token": uuid()})
+        self.assertEqual(expected, actual)
+
+    def test_session_invalid_token(self):
+        clear_users()
+        client: Client = get_client()
+        client.init()
+
+        expected = {"error": "invalid token"}
+        actual = client.request({"action": "session", "token": uuid()})
+        self.assertEqual(expected, actual)
+
+    def test_session_successful(self):
+        setup_account()
+        token = setup_session()
+        client: Client = get_client()
+        client.init()
+
+        expected = {"token": token}
+        actual = client.request({"action": "session", "token": token})
+        self.assertEqual(expected, actual)
 
     def test_logout_failed(self):
         setup_account()
