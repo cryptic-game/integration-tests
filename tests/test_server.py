c1c12f7c76b01e7c67b7fa67ed8e638d4b317587
Defelo <elodef42@gmail.com>
2020-07-18 20:06:36 +0200

    Added tests for user logout

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,9 +6,31 @@ from database import execute
 from util import get_client, is_uuid, uuid
 
 
+def clear_users():
+    execute("DELETE FROM user")
+
+
+def setup_account():
+    clear_users()
+    execute(
+        "INSERT INTO user (uuid, created, name, password) VALUES (%s, current_timestamp, 'super', %s)",
+        uuid(),
+        "$2a$12$qjj2fvAdX52pGZjWtbPnfOo0v5xqxZUTAoA7Ubbn1M/DF5QNJloLi",  # Super1234#
+    )
+
+
 class TestServer(TestCase):
+    def test_register_already_logged_in(self):
+        setup_account()
+        client: Client = get_client()
+        client.login("super", "Super1234#")
+
+        expected = {"error": "unknown action"}
+        actual = client.request({"action": "register", "name": "super", "password": "foo"})
+        self.assertEqual(expected, actual)
+
     def test_register_invalid_password(self):
-        execute("DELETE FROM user")
+        clear_users()
         client: Client = get_client()
         client.init()
 
@@ -17,8 +39,7 @@ class TestServer(TestCase):
         self.assertEqual(expected, actual)
 
     def test_register_username_already_exists(self):
-        execute("DELETE FROM user")
-        execute("INSERT INTO user (uuid, name) VALUES (%s, 'super')", uuid())
+        setup_account()
         client: Client = get_client()
         client.init()
 
@@ -27,7 +48,7 @@ class TestServer(TestCase):
         self.assertEqual(expected, actual)
 
     def test_register_successful(self):
-        execute("DELETE FROM user")
+        clear_users()
         client: Client = get_client()
         client.init()
 
@@ -36,8 +57,17 @@ class TestServer(TestCase):
         self.assertEqual(["token"], list(result))
         self.assertTrue(is_uuid(result["token"]))
 
-    def test_login_failed(self):
-        execute("DELETE FROM user")
+    def test_login_already_logged_in(self):
+        setup_account()
+        client: Client = get_client()
+        client.login("super", "Super1234#")
+
+        expected = {"error": "unknown action"}
+        actual = client.request({"action": "login", "name": "super", "password": "Super1234#"})
+        self.assertEqual(expected, actual)
+
+    def test_login_invalid_credentials(self):
+        clear_users()
         client: Client = get_client()
         client.init()
 
@@ -46,12 +76,7 @@ class TestServer(TestCase):
         self.assertEqual(expected, actual)
 
     def test_login_successful(self):
-        execute("DELETE FROM user")
-        execute(
-            "INSERT INTO user (uuid, name, password) VALUES (%s, 'super', %s)",
-            uuid(),
-            "$2a$12$qjj2fvAdX52pGZjWtbPnfOo0v5xqxZUTAoA7Ubbn1M/DF5QNJloLi",  # Super1234#
-        )
+        setup_account()
         client: Client = get_client()
         client.init()
 
@@ -59,3 +84,21 @@ class TestServer(TestCase):
         self.assertIsInstance(result, dict)
         self.assertEqual(["token"], list(result))
         self.assertTrue(is_uuid(result["token"]))
+
+    def test_logout_failed(self):
+        setup_account()
+        client: Client = get_client()
+        client.init()
+
+        expected = {"error": "unknown action"}
+        actual = client.request({"action": "logout"})
+        self.assertEqual(expected, actual)
+
+    def test_logout_successful(self):
+        setup_account()
+        client: Client = get_client()
+        client.login("super", "Super1234#")
+
+        expected = {"status": "logout"}
+        actual = client.request({"action": "logout"})
+        self.assertEqual(expected, actual)
