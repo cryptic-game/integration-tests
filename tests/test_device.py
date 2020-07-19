80397c3760da297f3e4b15a968022a7cf4d36516
Defelo <elodef42@gmail.com>
2020-07-20 00:17:28 +0200

    Added device info tests

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,7 +1,9 @@
+from typing import List
 from unittest import TestCase
 
 from PyCrypCli.client import Client
 from PyCrypCli.exceptions import AlreadyOwnADeviceException, DeviceNotFoundException
+from PyCrypCli.game_objects import Device
 
 from database import execute
 from tests.test_server import setup_account, super_password, super_uuid
@@ -64,3 +66,35 @@ class TestDevice(TestCase):
         self.assertEqual({"online": True}, self.client.ms("device", ["device", "ping"], device_uuid=device_uuid))
         execute("UPDATE device_device SET powered_on=false WHERE uuid=%s", device_uuid)
         self.assertEqual({"online": False}, self.client.ms("device", ["device", "ping"], device_uuid=device_uuid))
+
+    def test_info_not_found(self):
+        clear_devices()
+
+        with self.assertRaises(DeviceNotFoundException):
+            self.client.ms("device", ["device", "info"], device_uuid=device_uuid)
+
+    def test_info_successful(self):
+        clear_devices()
+        device = Device.starter_device(self.client)
+
+        result = self.client.ms("device", ["device", "info"], device_uuid=device.uuid)
+
+        self.assertIsInstance(result, dict)
+        self.assertEqual(["hardware", "name", "owner", "powered_on", "uuid"], sorted(result))
+        self.assertEqual(device.uuid, result["uuid"])
+        self.assertEqual(True, result["powered_on"])
+        self.assertEqual(super_uuid, result["owner"])
+        self.assertEqual(device.name, result["name"])
+
+        hardware: List[dict] = result["hardware"]
+        self.assertIsInstance(hardware, list)
+        self.assertEqual(7, len(hardware))
+        types = {"mainboard", "cpu", "processorCooler", "ram", "disk", "powerPack", "case"}
+        for element in hardware:
+            self.assertIsInstance(element, dict)
+            self.assertEqual(["device_uuid", "hardware_element", "hardware_type", "uuid"], sorted(element))
+            self.assertEqual(device.uuid, element["device_uuid"])
+            self.assertTrue(is_uuid(element["uuid"]))
+            self.assertIn(element["hardware_type"], types)
+            types.remove(element["hardware_type"])
+            self.assertRegex(element["hardware_element"], r"^[a-zA-Z0-9 .-]+$")
