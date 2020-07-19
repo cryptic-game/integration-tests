5a87ce7a5998ee209bc85694eff824ab9276d271
Defelo <elodef42@gmail.com>
2020-07-19 17:14:27 +0200

    Added tests for settings action

--- tests/test_server.py
+++ tests/test_server.py
@@ -43,132 +43,124 @@ def setup_session() -> str:
 
 
 class TestServer(TestCase):
+    def setUp(self):
+        self.client: Client = get_client()
+
+    def tearDown(self):
+        self.client.close()
+
     def test_register_already_logged_in(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
         expected = {"error": "unknown action"}
-        actual = client.request({"action": "register", "name": "super", "password": "foo"})
+        actual = self.client.request({"action": "register", "name": "super", "password": "foo"})
         self.assertEqual(expected, actual)
 
     def test_register_invalid_password(self):
         clear_users()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "invalid password"}
-        actual = client.request({"action": "register", "name": "super", "password": "foo"})
+        actual = self.client.request({"action": "register", "name": "super", "password": "foo"})
         self.assertEqual(expected, actual)
 
     def test_register_username_already_exists(self):
         setup_account()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "username already exists"}
-        actual = client.request({"action": "register", "name": "super", "password": super_password})
+        actual = self.client.request({"action": "register", "name": "super", "password": super_password})
         self.assertEqual(expected, actual)
 
     def test_register_successful(self):
         clear_users()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
-        result = client.request({"action": "register", "name": "super", "password": super_password})
+        result = self.client.request({"action": "register", "name": "super", "password": super_password})
         self.assertIsInstance(result, dict)
         self.assertEqual(["token"], list(result))
         self.assertTrue(is_uuid(result["token"]))
 
     def test_login_already_logged_in(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
         expected = {"error": "unknown action"}
-        actual = client.request({"action": "login", "name": "super", "password": super_password})
+        actual = self.client.request({"action": "login", "name": "super", "password": super_password})
         self.assertEqual(expected, actual)
 
     def test_login_invalid_credentials(self):
         clear_users()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "permissions denied"}
-        actual = client.request({"action": "login", "name": "super", "password": super_password})
+        actual = self.client.request({"action": "login", "name": "super", "password": super_password})
         self.assertEqual(expected, actual)
 
     def test_login_successful(self):
         setup_account()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
-        result = client.request({"action": "login", "name": "super", "password": super_password})
+        result = self.client.request({"action": "login", "name": "super", "password": super_password})
         self.assertIsInstance(result, dict)
         self.assertEqual(["token"], list(result))
         self.assertTrue(is_uuid(result["token"]))
 
     def test_session_already_logged_in(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
         expected = {"error": "unknown action"}
-        actual = client.request({"action": "session", "token": uuid()})
+        actual = self.client.request({"action": "session", "token": uuid()})
         self.assertEqual(expected, actual)
 
     def test_session_invalid_token(self):
         clear_users()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "invalid token"}
-        actual = client.request({"action": "session", "token": uuid()})
+        actual = self.client.request({"action": "session", "token": uuid()})
         self.assertEqual(expected, actual)
 
     def test_session_successful(self):
         setup_account()
         token = setup_session()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"token": token}
-        actual = client.request({"action": "session", "token": token})
+        actual = self.client.request({"action": "session", "token": token})
         self.assertEqual(expected, actual)
 
     def test_logout_failed(self):
         setup_account()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "unknown action"}
-        actual = client.request({"action": "logout"})
+        actual = self.client.request({"action": "logout"})
         self.assertEqual(expected, actual)
 
     def test_logout_successful(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
         expected = {"status": "logout"}
-        actual = client.request({"action": "logout"})
+        actual = self.client.request({"action": "logout"})
         self.assertEqual(expected, actual)
 
     def test_status_action(self):
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
-        result = client.request({"action": "status"})
+        result = self.client.request({"action": "status"})
         self.assertIsInstance(result, dict)
         self.assertEqual(["online"], list(result))
         self.assertGreaterEqual(result["online"], 1)
 
     def test_info_action(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
-        result = client.request({"action": "info"})
+        result = self.client.request({"action": "info"})
         self.assertIsInstance(result, dict)
         self.assertEqual(["created", "last", "name", "online", "uuid"], sorted(result))
         self.assertLess(abs(result["created"] / 1000 - time.time()), 10)
@@ -179,45 +171,103 @@ class TestServer(TestCase):
 
     def test_password_logged_in(self):
         setup_account()
-        client: Client = get_client()
-        client.login("super", super_password)
+        self.client.login("super", super_password)
 
         expected = {"error": "unknown action"}
-        actual = client.request({"action": "password", "name": "super", "password": super_password, "new": "x"})
+        actual = self.client.request({"action": "password", "name": "super", "password": super_password, "new": "x"})
         self.assertEqual(expected, actual)
 
     def test_password_invalid_credentials(self):
         clear_users()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "permissions denied"}
-        actual = client.request(
+        actual = self.client.request(
             {"action": "password", "name": "super", "password": super_password, "new": super_password + "x"}
         )
         self.assertEqual(expected, actual)
 
     def test_password_invalid_password(self):
         setup_account()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"error": "permissions denied"}
-        actual = client.request({"action": "password", "name": "super", "password": super_password, "new": "x"})
+        actual = self.client.request({"action": "password", "name": "super", "password": super_password, "new": "x"})
         self.assertEqual(expected, actual)
 
     def test_password_successful(self):
         setup_account()
-        client: Client = get_client()
-        client.init()
+        self.client.init()
 
         expected = {"success": True}
-        actual = client.request(
+        actual = self.client.request(
             {"action": "password", "name": "super", "password": super_password, "new": super_password + "x"}
         )
         self.assertEqual(expected, actual)
 
         with self.assertRaises(InvalidLoginException):
-            client.login("super", super_password)
+            self.client.login("super", super_password)
 
-        self.assertTrue(is_uuid(client.login("super", super_password + "x")))
+        self.assertTrue(is_uuid(self.client.login("super", super_password + "x")))
+
+    def test_settings_not_logged_in(self):
+        clear_users()
+        self.client.init()
+
+        expected = {"error": "unknown action"}
+        actual = self.client.request({"action": "setting", "key": "foo", "value": "bar"})
+        self.assertEqual(expected, actual)
+
+    def test_settings_add_too_long(self):
+        setup_account()
+        self.client.login("super", super_password)
+
+        expected = {"error": "unsupported parameter size"}
+        actual = self.client.request({"action": "setting", "key": "foo", "value": "A" * 2048})
+        self.assertEqual(expected, actual)
+
+    def test_settings_add_successful(self):
+        setup_account()
+        self.client.login("super", super_password)
+
+        expected = {"key": "foo", "value": "bar"}
+        actual = self.client.request({"action": "setting", "key": "foo", "value": "bar"})
+        self.assertEqual(expected, actual)
+
+    def test_settings_get_not_found(self):
+        setup_account()
+        execute("DELETE FROM user_settings")
+        self.client.login("super", super_password)
+
+        expected = {"error": "unknown setting"}
+        actual = self.client.request({"action": "setting", "key": "foo"})
+        self.assertEqual(expected, actual)
+
+    def test_settings_get_successful(self):
+        setup_account()
+        execute("DELETE FROM user_settings")
+        execute("INSERT INTO user_settings (user, settingKey, settingValue) VALUES (%s, 'foo', 'bar')", super_uuid)
+        self.client.login("super", super_password)
+
+        expected = {"key": "foo", "value": "bar"}
+        actual = self.client.request({"action": "setting", "key": "foo"})
+        self.assertEqual(expected, actual)
+
+    def test_settings_delete_not_found(self):
+        setup_account()
+        execute("DELETE FROM user_settings")
+        self.client.login("super", super_password)
+
+        expected = {"error": "unknown setting"}
+        actual = self.client.request({"action": "setting", "key": "foo", "delete": ""})
+        self.assertEqual(expected, actual)
+
+    def test_settings_delete_successful(self):
+        setup_account()
+        execute("DELETE FROM user_settings")
+        execute("INSERT INTO user_settings (user, settingKey, settingValue) VALUES (%s, 'foo', 'bar')", super_uuid)
+        self.client.login("super", super_password)
+
+        expected = {"success": True}
+        actual = self.client.request({"action": "setting", "key": "foo", "delete": ""})
+        self.assertEqual(expected, actual)
